f4c91814622c46fee53bfce35adcb5683f7fd6c2
SydneySky1 <ceced10@gmail.com>
2021-04-17 23:22:27 -0400

    updated

--- script.js
+++ script.js
@@ -1,60 +1,63 @@
-const p1Button = document.querySelector('.btn-player1');
-const p2Button = document.querySelector('.btn-player2');
-const p1Display = document.querySelector('.player1-score');
-const p2Display = document.querySelector('.player2-score');
+
+const p1 = {
+  score: 0,
+  button: document.querySelector('.btn-player1'),
+  display: document.querySelector('.player1-score')
+}
+
+const p2 = {
+  score: 0,
+  button: document.querySelector('.btn-player2'),
+  display: document.querySelector('.player2-score')
+}
+
 const btnReset = document.querySelector('.btn-reset');
 const winningScoreSelect = document.querySelector('#playTo');
 
-
-let p1Score = 0;
-let p2Score = 0;
 let winningScore = 5;
 let isGameOver = false;
 
-//BUTTON ONE SCORE KEEPER
+function updateScores(player, opponent) {
+      if (!isGameOver){
+        player.score += 1;
+      if(player.score === winningScore){
+      isGameOver = true;
+      player.display.classList.add('winner');
+      opponent.display.classList.add('loser');
+      player.button.disabled = true;
+      opponent.button.disabled = true;
+    }
+       player.display.innerText = player.score;
+    }
+  }
+
 //IF ISGAMEOVER IS TRUE, CODE STOPS RUNNING
-p1Button.addEventListener('click', function() {
-  if (!isGameOver){
-    p1Score +=1;
-  if(p1Score === winningScore){
- isGameOver = true;
- p1Display.classList.add('winner');
- p2Display.classList.add('loser');
-  }
- p1Display.innerText = p1Score;
-}
+p1.button.addEventListener('click', function() {
+updateScores(p1, p2);
 });
 
-//BUTTON TWO SCORE KEEPER
-p2Button.addEventListener('click', function() {
-  if (!isGameOver){
-    p2Score +=1;
-  if(p2Score === winningScore){
- isGameOver = true;
-  p2Display.classList.add('winner');
-  p1Display.classList.add('loser');
-  }
- p2Display.innerText = p2Score;
-}
+p2.button.addEventListener('click', function() {
+ updateScores(p2, p1);
 });
 
-//THIS REFERS TO THE OBJECT EVENT WE ARE CURRENTLY LISTENING FOR, AND WE ALWAYS HAVE ACCESS TO THE VALUE OF THAT OBJECT EVENT
+//THIS REFERS TO THE OBJECT EVENT WE ARE CURRENTLY LISTENING FOR
+//WE ALWAYS HAVE ACCESS TO THE VALUE OF THAT OBJECT EVENT
 playTo.addEventListener('change', function(){
-//CONVERTS WINNINGSCORE VALUE, WHICH IS A STRING, INTO A WHOLE INTEGER. CHECK VARIABLE IN CONSOLE TO VERIFY DATA OUTPUT AS A NUMBER OR STRING
+//CONVERTS WINNINGSCORE VALUE, WHICH IS A STRING, INTO A WHOLE INTEGER. 
+//CHECK VARIABLE IN CONSOLE TO VERIFY DATA OUTPUT AS A NUMBER OR STRING
  winningScore = parseInt(this.value);
  reset();
 });
 
-//PASSING resetGame AS A REFERENCE/PARAMETER AND IT WILL EXECUTE WHEN THE TIME COMES
+//PASSING reset AS A REFERENCE/PARAMETER AND IT WILL EXECUTE WHEN THE TIME COMES
 btnReset.addEventListener('click', reset)
-//BUTTON RESET
-//IF ISGAMEOVER IS TRUE, CODE STOPS RUNNING
+
 function reset(){
   isGameOver = false;
-  p1Score = 0;
-  p2Score = 0;
-  p1Display.innerText = 0;
-  p2Display.innerText = 0;
-  p1Display.classList.remove('winner', 'loser');
-  p2Display.classList.remove('winner', 'loser');
+  for(let p of [p1,p2]){
+    p.score=0;
+    p.display.innerText = 0;
+    p.display.classList.remove('winner', 'loser');
+    p.button.disabled = false;
+  }
 };
